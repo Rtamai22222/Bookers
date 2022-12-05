02900c8fb645b75555e46e8ae754d9156691dc01
EC2 Default User <ec2-user@ip-172-31-20-212.ap-northeast-1.compute.internal>
2022-12-05 06:13:19 +0000

    [Add] 投稿機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'books' => 'books#index'
+  get 'books/show'
+  get 'books/edit'
+  post 'books' => 'books#create'
+  # resources :books
+  root to: 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
