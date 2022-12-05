410c3bf50d18a9a0f33d408bf01f77cd75559a06
EC2 Default User <ec2-user@ip-172-31-20-212.ap-northeast-1.compute.internal>
2022-12-05 12:54:18 +0000

    [Add] 削除機能の追加、その他

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,23 +3,6 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
-  # def create
-  #   book = Book.new(book_params)
-  #   book.save
-  #   redirect_to '/books/'
-  # end
-  
-  def create
-    book = Book.new(book_params)
-    if book.save
-      # flash[:notice] = "投稿が成功しました"
-      redirect_to list_path(book.id)
-    else
-      # @lists = List.all
-      render :new
-    end
-  end
-
 
   def index
     @books = Book.all
@@ -27,15 +10,39 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
+
   def show
     @book = Book.find(params[:id])
   end
+
 
   def edit
     @book = Book.find(params[:id])
   end
 
+  
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to '/books'
+  end
+
+  
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      # flash[:notice] = "投稿が成功しました"
+      redirect_to book_path(@book.id)
+    else
+      # @lists = List.all
+      render :new
+    end
+  end
+  
+
   private
+  
+  
   def book_params
     params.require(:book).permit(:title, :body)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'books' => 'books#index'
-  get 'books/:id' => 'books#show', as: 'list'
-  get 'books/:id/edit' => 'books#edit'
-  post 'books' => 'books#create'
-  # resources :books
+  # get 'books' => 'books#index'
+  # get 'books/:id' => 'books#show', as: 'list'
+  # get 'books/:id/edit' => 'books#edit'
+  # post 'books' => 'books#create'
   root to: 'homes#top'
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
