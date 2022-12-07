2d46741f0b85fcde494a0c989994eea951088dd5
EC2 Default User <ec2-user@ip-172-31-20-212.ap-northeast-1.compute.internal>
2022-12-07 09:22:04 +0000

    [Add] エラーメッセージとサクセスメッセージの追加、RSpecのインストール

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,37 +18,41 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-  
-  
+
+
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "successfully updated"
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
-  
+
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
     redirect_to '/books'
   end
 
-  
+
   def create
     @book = Book.new(book_params)
     if @book.save
-      # flash[:notice] = "投稿が成功しました"
+      flash[:notice] = "successfully created"
       redirect_to book_path(@book.id)
     else
       @books = Book.all
       render :index
     end
   end
-  
+
 
   private
-  
-  
+
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
